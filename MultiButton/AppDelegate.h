f4234481ff0bc8bb467a8408d075e1509471a632
qianlishun <511940597@qq.com>
2016-10-13 12:33:10 +0800

    Initial Commit

---
+++ MultiButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiButton
+//
+//  Created by mrq on 16/10/13.
+//  Copyright © 2016年 MrQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiButton
+//
+//  Created by mrq on 16/10/13.
+//  Copyright © 2016年 MrQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
